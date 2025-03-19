58e7b135f72c23e40be63a5c719a6c90153c382f
Shaban Hussein <Shabanindekupe@gmail.com>
2025-03-19 03:13:17 -0700

    Initial commit

---
+++ shabani/script.js
@@ -0,0 +1,21 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Form validation (basic)
+document.querySelector('form').addEventListener('submit', function (e) {
+    const name = this.querySelector('input[type="text"]').value;
+    const email = this.querySelector('input[type="email"]').value;
+    const message = this.querySelector('textarea').value;
+
+    if (!name || !email || !message) {
+        alert('Please fill in all fields');
+        e.preventDefault();
+    }
+});
